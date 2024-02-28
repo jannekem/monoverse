5223177f56b8e4f18dd0ce089c5250bc51f9ffbc
Janne Kemppainen <kemppainen.janne@gmail.com>
2024-02-28 20:42:23 +0200

    refactor: use edit::yaml for helm project

--- src/projects/helm.rs
+++ src/projects/helm.rs
@@ -1,6 +1,4 @@
 use anyhow::Result;
-use regex::RegexBuilder;
-use serde_yaml::Value;
 use std::path::PathBuf;
 
 use crate::{
@@ -29,30 +27,8 @@ impl super::ProjectFile for HelmProject {
     }
 
     fn version_context(&self, version_file_content: &str) -> Result<VersionContext> {
-        let value: Value = serde_yaml::from_str(version_file_content)?;
-        let version = value["appVersion"]
-            .as_str()
-            .ok_or(anyhow::anyhow!(
-                "Failed to parse version from Chart.yaml: {:?}",
-                self.base.settings.get_manifest_file_path()
-            ))?
-            .to_version();
-        let pattern = regex::Regex::new(&format!(r#"^appVersion:\s*"?{}"?"#, version))?;
-        let line_number = version_file_content
-            .lines()
-            .enumerate()
-            .find(|(_, line)| pattern.is_match(line))
-            .map(|(line_number, _)| line_number + 1)
-            .ok_or(anyhow::anyhow!(
-                "Failed to find version in Chart.yaml: {:?}",
-                self.base.settings.get_manifest_file_path()
-            ))?;
-        log::info!(
-            "Found version {} in Chart.yaml at line {}",
-            version,
-            line_number
-        );
-        Ok(VersionContext::new(version, line_number))
+        let line_context = crate::edit::yaml::query(version_file_content, "appVersion")?;
+        Ok(VersionContext::from_line_context(line_context))
     }
 
     fn update_version(
@@ -60,32 +36,79 @@ impl super::ProjectFile for HelmProject {
         version_file_content: &str,
         version_context: &VersionContext,
     ) -> Result<String> {
-        let value: Value = serde_yaml::from_str(version_file_content)?;
-        let chart_version = value["version"]
-            .as_str()
-            .ok_or(anyhow::anyhow!(
-                "Failed to parse version from Chart.yaml: {:?}",
-                self.base.settings.get_manifest_file_path()
-            ))?
-            .to_version();
-        let app_version_pattern = RegexBuilder::new(&format!(
-            r#"^appVersion:\s*"?{}"?"#,
-            version_context.version
-        ))
-        .multi_line(true)
-        .build()?;
-        let chart_version_pattern =
-            RegexBuilder::new(&format!(r#"^version:\s*"?{}"?"#, chart_version))
-                .multi_line(true)
-                .build()?;
-        let result = app_version_pattern.replace(
-            &version_file_content,
-            format!(r#"appVersion: "{}""#, version_context.next_version),
+        let next_chart_version = crate::edit::yaml::query(version_file_content, "version")?
+            .value
+            .to_version()
+            .bump_patch()
+            .to_string();
+        let next_app_version = version_context.next_version.to_string();
+        let mut new_content =
+            crate::edit::yaml::edit(version_file_content, "version", &next_chart_version)?;
+        new_content = crate::edit::yaml::edit(&new_content, "appVersion", &next_app_version)?;
+        Ok(new_content)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::projects::ProjectFile;
+    use crate::version::ToVersion;
+
+    #[test]
+    fn test_version_context() {
+        let project = HelmProject::new(
+            ProjectSettings {
+                project_type: crate::projects::ProjectType::Helm,
+                project_path: PathBuf::new(),
+                manifest_path: Some("Chart.yaml".into()),
+                selector: None,
+                dependents: None,
+            },
+            PathBuf::new(),
         );
-        let result = chart_version_pattern.replace(
-            &result,
-            format!(r#"version: {}"#, chart_version.bump_patch()),
+        let version_file_content = r#"apiVersion: v2
+appVersion: "1.2.3"
+description: A Helm chart for Kubernetes
+name: test
+version: 0.1.0"#;
+        let result = project.version_context(version_file_content).unwrap();
+        assert_eq!(result.version, "1.2.3".to_version());
+        assert_eq!(result.line_number, 2);
+    }
+
+    #[test]
+    fn test_update_version() {
+        let project = HelmProject::new(
+            ProjectSettings {
+                project_type: crate::projects::ProjectType::Helm,
+                project_path: PathBuf::new(),
+                manifest_path: Some("Chart.yaml".into()),
+                selector: None,
+                dependents: None,
+            },
+            PathBuf::new(),
         );
-        Ok(result.into_owned())
+        let version_file_content = r#"apiVersion: v2
+description: A Helm chart for Kubernetes
+name: test
+appVersion: "1.2.3"
+version: 0.1.0"#;
+        let version_context = VersionContext {
+            version: "1.2.3".to_version(),
+            next_version: "1.2.4".to_version(),
+            line_number: 3,
+        };
+        let result = project
+            .update_version(version_file_content, &version_context)
+            .unwrap();
+        assert_eq!(
+            result,
+            r#"apiVersion: v2
+description: A Helm chart for Kubernetes
+name: test
+appVersion: "1.2.4"
+version: 0.1.1"#
+        );
     }
 }
